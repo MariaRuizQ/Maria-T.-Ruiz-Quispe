ddf9407a12e486f5b1daa951ff68bba48914bd2f
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2018-03-14 10:11:23 -0500

    Actualizando y añadiendo

--- js/scroll.js
+++ js/scroll.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
   });
+
+  $(function() {
+  $('scroll-2').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 400, 'linear');
+  });
+  });
 });
